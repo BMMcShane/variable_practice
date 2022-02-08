43d7a6fb4b6eee2c82aba48ce3a5ec213295ce32
Ben McShane <benjaminmartinmc@gmail.com>
2022-02-07 20:20:13 -0700

    Add annotation

--- variables.js
+++ variables.js
@@ -1,8 +1,14 @@
+// variable textEditor is declared and assigned to the String of "atom"
 var textEditor = "atom"
+// variable computer is declared and assigned to the String of "Mac"
 var computer = "Mac"
 
+// variable areaCode is declared and assigned to the Number of 417
 var areaCode = 417
+// variable dayOfMonth is declared and assigned to the number of 7
 var dayOfMonth = 7
 
+// variable content is declared and assigned to the Boolean of true
 var content = true
+// variable filthyRich is declared and assigned to the Boolean of false
 var filthyRich = false
